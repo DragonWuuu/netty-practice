65cf176c7ba5a02d0450c9b6e4c890cc93d8afcc
DragonWuuu <3350986682@qq.com>
2025-08-29 01:35:20 +0800

    feature:implemented POJO Time server

---
+++ time-server/src/main/java/com/dragon/practice/TimeDecoder.java
@@ -0,0 +1,19 @@
+package com.dragon.practice;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+        out.add(new UnixTime(in.readUnsignedInt())); // (4)
+    }
+}
