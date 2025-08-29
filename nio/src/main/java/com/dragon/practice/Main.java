963bed3f1ee498dbe14b9b921fadd1493ae67557
DragonWuuu <3350986682@qq.com>
2025-08-29 15:52:45 +0800

    feat:use transfer method of channel copy data

--- nio/src/main/java/com/dragon/practice/Main.java
+++ nio/src/main/java/com/dragon/practice/Main.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -11,22 +12,16 @@ import java.nio.channels.FileChannel;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        ByteBuffer buffer = ByteBuffer.allocate(10);
-        buffer.put((byte) 0x61);
-        buffer.put((byte) 0x62);
-        // 在不flip的情况下读数据：
-//        log.info("在不flip的情况下读数据：{}", (char) buffer.get());
-        // 反转后读数据
-        buffer.flip();
-        log.info("反转后读数据：{}", (char) buffer.get());
-        // 压缩后
-        buffer.compact();
-        // 此时读数据看看是不是读到了没被删除的0x62
-        log.info("查看position：{}", buffer.position());
-        log.info("压缩后读数据：{}", (char) buffer.get());
-        log.info("查看position：{}", buffer.position());
-        log.info("使用get(1)方法读取数据：{}", (char) buffer.get(1));
-        log.info("查看position：{}", buffer.position());
-        log.info("总结：谨慎使用get()方法，应当在调用flip()方法后使用，但使用get(1)方法不会移动指针");
+        // 从一个文件copy到另一个文件，现在有两个文件：resource.txt、target.txt
+        try {
+            FileChannel resourceChannel = new FileInputStream("./nio/src/main/resources/resource.txt").getChannel();
+            FileChannel targetChannel = new FileOutputStream("./nio/src/main/resources/target.txt").getChannel();
+            resourceChannel.transferTo(0, resourceChannel.size(), targetChannel);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
