e4fb2f37984960a50568dc4a0e2f5e71f46d6b39
DragonWuuu <3350986682@qq.com>
2025-08-30 20:59:02 +0800

    feat:block mode test

---
+++ nio/src/main/java/com/dragon/practice/Client.java
@@ -0,0 +1,20 @@
+package com.dragon.practice;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+@Slf4j
+public class Client {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("localhost", 8080));
+        log.info("Connected to server {}", socketChannel.getRemoteAddress());
+        socketChannel.write(ByteBuffer.wrap("Hello".getBytes()));
+        socketChannel.close();
+
+    }
+}
--- nio/src/main/java/com/dragon/practice/Main.java
+++ nio/src/main/java/com/dragon/practice/Main.java
@@ -2,26 +2,55 @@ package com.dragon.practice;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        // 从一个文件copy到另一个文件，现在有两个文件：resource.txt、target.txt
+        // 使用NIO理解阻塞模式
+
+        // buffer
+        ByteBuffer buffer = ByteBuffer.allocate(16);
         try {
-            FileChannel resourceChannel = new FileInputStream("./nio/src/main/resources/resource.txt").getChannel();
-            FileChannel targetChannel = new FileOutputStream("./nio/src/main/resources/target.txt").getChannel();
-            resourceChannel.transferTo(0, resourceChannel.size(), targetChannel);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            // 使用NIO创建服务器
+            ServerSocketChannel  serverSocketChannel = ServerSocketChannel.open();
+            log.info("opening serverSocketChannel");
+            // 绑定监听端口
+            serverSocketChannel.bind(new InetSocketAddress(8080));
+            log.info("bing port 8080");
+            // 建立多客户端的连接
+            List<SocketChannel> channels = new ArrayList<>();
+            do {
+                // accept默认是阻塞的
+                SocketChannel channel = serverSocketChannel.accept();
+                log.info("accept client: {}", channel);
+                channels.add(channel);
+                // 读数据
+                for (SocketChannel socketChannel : channels) {
+                    // read 也是阻塞的
+                    socketChannel.read(buffer);
+                    buffer.flip();
+                    while (buffer.hasRemaining()) {
+                        log.info("{}", (char) buffer.get());
+                    }
+                    buffer.clear();
+                }
+            } while (true);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
