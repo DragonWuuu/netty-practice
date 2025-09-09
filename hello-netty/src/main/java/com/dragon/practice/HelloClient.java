9ff88429c7089993e7d451d72683ddb7e25831ee
DragonWuuu <3350986682@qq.com>
2025-09-10 01:18:49 +0800

    feat:some function test

--- hello-netty/src/main/java/com/dragon/practice/HelloClient.java
+++ hello-netty/src/main/java/com/dragon/practice/HelloClient.java
@@ -1,35 +1,78 @@
 package com.dragon.practice;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioIoHandler;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 @Slf4j
 public class HelloClient {
     public static void main(String[] args) throws InterruptedException {
-        Channel channel = new Bootstrap()
-                .group(new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory()))
+        MultiThreadIoEventLoopGroup group = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
+        ChannelFuture channelFuture = new Bootstrap()
+                .group(group)
                 .channel(NioSocketChannel.class)
                 .handler(
                         new ChannelInitializer<>() {
                             @Override
                             protected void initChannel(Channel channel) throws Exception {
                                 channel.pipeline()
+                                        .addLast(new LoggingHandler(LogLevel.DEBUG))
                                         .addLast(new StringEncoder());
                             }
                         }
                 )
-                .connect(new InetSocketAddress("localhost", 8080))
+                .connect(new InetSocketAddress("localhost", 8080));
+
+        Channel channel = channelFuture
                 // 等待连接建立
                 .sync()
                 .channel();
-        channel.writeAndFlush("Hello,world");
+
+        // 使用监听者模式，对比起sync()方法，这个不会阻塞主线程，operationComplete方法在NIO线程执行
+//        channelFuture.addListener(new ChannelFutureListener() {
+//            @Override
+//            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+//                Channel channel = channelFuture.channel();
+//                channel.writeAndFlush("hello,world");
+//            }
+//        });
+        // 创建一个新的Thread，将console的消息发送到服务器，如果输入为q，则退出。
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String line = scanner.nextLine();
+                if (line.equals("q")){
+                    // 异步操作
+                    channel.close();
+                    log.info("close?");
+                    break;
+                }
+                // 发送数据
+                channel.writeAndFlush(line);
+            }
+        }, "scanner").start();
+
+        // 使用CloseFuture在channel关闭后执行代码
+        ChannelFuture closeFuture = channel.closeFuture();
+        log.info("wait for channel close...");
+
+        // 使用监听器的方式
+        closeFuture.addListener(future -> {
+            log.info("listened channel closed");
+        });
+
+        // 使用同步的方式
+        closeFuture.sync();
+        log.info("sync channel closed");
+
+        group.shutdownGracefully();
     }
 }
--- hello-netty/src/main/java/com/dragon/practice/HelloServer.java
+++ hello-netty/src/main/java/com/dragon/practice/HelloServer.java
@@ -1,6 +1,8 @@
 package com.dragon.practice;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioIoHandler;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -9,24 +11,55 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class HelloServer {
-    public static void main(String[] args) {
-        new ServerBootstrap()
-                .group(new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory()))
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<>() {
-                    @Override
-                    protected void initChannel(Channel channel) throws Exception {
-                        channel.pipeline()
-                                .addLast(new StringDecoder())
-                                .addLast(new ChannelInboundHandlerAdapter() {
-                                    @Override
-                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                        log.info("{}", msg);
-                                    }
-                                });
-                    }
-                })
-                .bind(8080);
+    public static void main(String[] args){
+        MultiThreadIoEventLoopGroup boss = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
+        MultiThreadIoEventLoopGroup worker = new MultiThreadIoEventLoopGroup(4, NioIoHandler.newFactory());
+
+        try {
+            ChannelFuture channelFuture = new ServerBootstrap()
+                    .group(
+                            // boss
+                            boss,
+                            // worker
+                            worker
+                    )
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<>() {
+                        @Override
+                        protected void initChannel(Channel channel) throws Exception {
+                            ChannelPipeline pipeline = channel.pipeline();
+                            pipeline.addLast("StringDecoder", new StringDecoder())
+                                    .addLast(new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                            log.info("{}", msg);
+                                            ctx.channel().writeAndFlush(ctx.alloc().buffer().writeBytes("...".getBytes()));
+                                            ctx.writeAndFlush("...");
+                                            super.channelRead(ctx, msg);
+                                        }
+
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                            log.info("active: {}", ctx.channel());
+                                        }
+                                    });
+                        }
+                    })
+                    .bind(8080);
+            // 等待服务建立完成
+            channelFuture.sync();
+            // 等待channel结束事件
+            Channel channel = channelFuture.channel();
+            channel.closeFuture().sync();
+            ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            // 优雅地关闭boss和worker
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
 
     }
 }
